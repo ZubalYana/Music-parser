4a82cc933da15e66443f08efa43012ee595e4cf5
ZubalYana <139336847+ZubalYana@users.noreply.github.com>
2024-12-14 16:46:17 +0200

    get and display the music on the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import axios from 'axios';
 
 function App() {
 
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios.get(`http://localhost:3000/music`).then((response) => {
+      console.log(response.data);
+      setData(response.data);
+    });
+  }, []);
+
   return (
     <>
+      {data.map((item, index) => (
+        <div className="musicEl" key={index}>
+          <h4>{item.title}</h4>
+          <audio src={item.link} controls></audio>
+        </div>
+      ))}
     </>
-  )
+  );
+  
 }
 
 export default App
+
